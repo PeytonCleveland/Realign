0fd59b6bb75b5b41d29e09921b874815db38c696
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-11 10:49:44 -0500

    header cleanup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -33,7 +33,7 @@ const Header = async () => {
           {user ? (
             <div className="flex items-center gap-4">
               <div className="flex items-center gap-4">
-                <div className="relative">
+                {/* <div className="relative">
                   <label className="sr-only" htmlFor="search">
                     Search
                   </label>
@@ -65,9 +65,9 @@ const Header = async () => {
                       />
                     </svg>
                   </button>
-                </div>
+                </div> */}
 
-                <a
+                {/* <a
                   href="#"
                   className="block shrink-0 rounded-full bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
                 >
@@ -86,7 +86,7 @@ const Header = async () => {
                       d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                     />
                   </svg>
-                </a>
+                </a> */}
               </div>
 
               <span
