e55c11206d7b25c2c794942474e7a1b8d810211b
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-13 15:05:00 -0500

    external

--- src/app/app/learn/page.tsx
+++ src/app/app/learn/page.tsx
@@ -45,12 +45,30 @@ const Learn = async () => {
               <Link
                 key={item.slug}
                 href={item.external_url ?? item.slug}
-                className="flex flex-col w-1/4 border-2 border-gray-300 rounded-md shadow-sm overflow-hidden"
+                className="flex flex-col w-1/4 border-2 border-gray-200 rounded-md shadow-sm overflow-hidden hover:border-blue-600"
               >
-                <div className="w-full h-[150px] relative object-cover">
+                <div className="w-full h-[150px] relative object-contain">
                   <Image src={item.thumbnail_url} fill alt={item.name} />
                 </div>
-                <h6>{item.name}</h6>
+                <div className="flex flex-col gap-2 p-3 w-full">
+                  <div className="flex justify-between items-center w-full">
+                    <h6>{item.name}</h6>
+                    {item.external_url ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6 text-gray-600"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : null}
+                  </div>
+                </div>
               </Link>
             );
           })}
