bbeb9acffd32c7b3d95addef070e7503c467b8a6
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-13 14:30:52 -0500

    courses

--- src/app/app/learn/page.tsx
+++ src/app/app/learn/page.tsx
@@ -3,7 +3,6 @@ import { Breadcrumbs, Page } from "@/components";
 const Learn = () => {
   return (
     <Page>
-      {" "}
       <div className="w-full bg-gray-50">
         <div className="mb-6 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 w-full">
           <Breadcrumbs
@@ -21,6 +20,26 @@ const Learn = () => {
             Learn about large language models, Realign, and Ocelot.&nbsp;
             <span className="text-lg">🧠</span>
           </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full gap-4 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 justify-between">
+        <p className="text-lg font-bold text-gray-900 sm:text-xl">
+          Featured Training
+        </p>
+        <div className="flex w-full gap-8 justify-between">
+          <div className="flex flex-1 border-2 border-gray-300 rounded-md shadow-sm">
+            <h6>Course Name</h6>
+          </div>
+          <div className="flex flex-1 border-2 border-gray-300 rounded-md shadow-sm">
+            <h6>Course Name</h6>
+          </div>
+          <div className="flex flex-1 border-2 border-gray-300 rounded-md shadow-sm">
+            <h6>Course Name</h6>
+          </div>
+          <div className="flex flex-1 border-2 border-gray-300 rounded-md shadow-sm">
+            <h6>Course Name</h6>
+          </div>
         </div>
       </div>
     </Page>
