cd2b20521ef8634005a7fd1135a444c5776cbefe
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-13 11:10:41 -0500

    margin

--- src/app/app/submissions/new/page.tsx
+++ src/app/app/submissions/new/page.tsx
@@ -296,7 +296,7 @@ const NewSubmission = () => {
       </div>
       <div className="flex w-full flex-col gap-8 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
         <div className="w-full flex flex-col gap-4">
-          <div className="w-full px-6 py-4 flex gap-8 justify-between items-center rounded-md shadow-sm bg-yellow-50 border-2 border-yellow-400">
+          <div className="w-full px-6 py-4 flex mb-2 gap-8 justify-between items-center rounded-md shadow-sm bg-yellow-50 border-2 border-yellow-400">
             <div className="flex gap-2 items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
