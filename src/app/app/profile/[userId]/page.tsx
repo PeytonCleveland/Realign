c552a369d9c570e8e3e60a32dd14ba1a2268c7be
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-16 14:40:38 -0500

    edit image

--- src/app/app/profile/[userId]/page.tsx
+++ src/app/app/profile/[userId]/page.tsx
@@ -9,6 +9,11 @@ const Profile = async ({ params }: { params: { userId: string } }) => {
   });
 
   const { userId } = params;
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   const { data: profiles } = await supabase
     .from("profiles")
     .select()
@@ -28,7 +33,21 @@ const Profile = async ({ params }: { params: { userId: string } }) => {
             className="mb-12"
           />
           <div className="flex items-center w-full">
-            <Avatar imageUrl={profiles[0].avatar_url} />
+            <div className="relative">
+              <Avatar imageUrl={profiles[0].avatar_url} />
+              {user?.id == userId ? (
+                <div className="absolute left-10 bg-blue-600 rounded-md shadow-sm p-1 bottom-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5 text-white"
+                  >
+                    <path d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+                  </svg>
+                </div>
+              ) : null}
+            </div>
             <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
               {profiles[0].full_name}
             </h1>
