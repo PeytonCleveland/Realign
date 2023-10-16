385750b3653ae23974859933f47e8b7c673267fb
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-16 11:08:55 -0500

    papers

--- src/app/app/learn/page.tsx
+++ src/app/app/learn/page.tsx
@@ -21,6 +21,10 @@ const Learn = async () => {
     return item.category === "training";
   });
 
+  const papers = trainingItems?.filter((item) => {
+    return item.category === "papers";
+  });
+
   return (
     <Page>
       <div className="w-full bg-gray-50">
@@ -95,6 +99,53 @@ const Learn = async () => {
         </p>
         <div className="flex w-full gap-8">
           {training?.map((item) => {
+            return (
+              <Link
+                key={item.slug}
+                target={item.external_url ? "_blank" : undefined}
+                href={item.external_url ?? item.slug}
+                className="flex flex-col w-1/4 border-2 border-gray-200 rounded-md shadow-sm overflow-hidden hover:border-blue-600"
+              >
+                <div className="w-full h-[150px] relative object-contain">
+                  <Image src={item.thumbnail_url} fill alt={item.name} />
+                </div>
+                <div className="flex flex-col gap-2 p-3 w-full">
+                  <div className="flex justify-between items-center w-full">
+                    <h6 className="text-gray-900 font-semibold text-sm">
+                      {item.name}
+                    </h6>
+                    {item.external_url ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-5 h-5 text-gray-500"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z"
+                          clipRule="evenodd"
+                        />
+                        <path
+                          fillRule="evenodd"
+                          d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : null}
+                  </div>
+                  <p className="text-xs font-light text-gray-600">
+                    {item.description.slice(0, 75)}...
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+
+        <p className="font-bold text-gray-900 sm:text-xl mt-4">White Papers</p>
+        <div className="flex w-full gap-8">
+          {papers?.map((item) => {
             return (
               <Link
                 key={item.slug}
