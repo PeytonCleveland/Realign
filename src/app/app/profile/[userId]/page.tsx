6a7b8c573716ba71028314ddc9c32c7b9f3ec459
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-16 13:05:50 -0500

    profile

--- src/app/app/profile/[userId]/page.tsx
+++ src/app/app/profile/[userId]/page.tsx
@@ -1,26 +1,42 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { Page } from "@/components";
+import { redirect } from "next/navigation";
+import { Avatar, Breadcrumbs, Page } from "@/components";
 
 const Profile = async ({ params }: { params: { userId: string } }) => {
   const supabase = createServerComponentClient({
     cookies,
   });
 
-  console.log("params: ", params);
-
   const { userId } = params;
-  console.log("userId: ", userId);
   const { data: profiles } = await supabase
     .from("profiles")
     .select()
     .eq("id", userId);
 
-  console.log("profiles: ", profiles);
+  if (!profiles || !profiles[0]) redirect("/app/404");
 
-  if (!profiles || !profiles[0]) return null;
-
-  return <Page>{profiles[0].full_name}</Page>;
+  return (
+    <Page>
+      <div className="w-full bg-gray-50">
+        <div className="mb-6 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 w-full">
+          <Breadcrumbs
+            items={[
+              { text: "Home", href: "/app" },
+              { text: "Profile", href: `/app/profile/${userId}`, active: true },
+            ]}
+            className="mb-12"
+          />
+          <div className="flex gap-3 items-center w-full">
+            <Avatar imageUrl={profiles[0].avatar_url} />
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              {profiles[0].full_name}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </Page>
+  );
 };
 
 export default Profile;
