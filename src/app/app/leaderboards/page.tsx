81d49c8a523b8a95b5d5cd5f26cb682ea6bcbd41
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-16 14:42:28 -0500

    remove underline

--- src/app/app/leaderboards/page.tsx
+++ src/app/app/leaderboards/page.tsx
@@ -73,7 +73,7 @@ const Leaderboards = async () => {
                 className="w-full flex items-center justify-between px-6 py-4 bg-gray-50 border-b-2 border-gray-100"
               >
                 <div className="flex gap-4 items-center">
-                  <h2 className="text-lg font-semibold text-gray-900 hover:underline underline-offset-2">
+                  <h2 className="text-lg font-semibold text-gray-900">
                     {index + 1}.&nbsp;&nbsp;{rank["full_name"]}
                   </h2>
                   <Link href={`/app/profile/${rank.id}`}>
