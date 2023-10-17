dc064d75f683f18895640f10fbbd790224e963a0
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-17 12:41:02 -0500

    article

--- src/app/app/learn/[itemSlug]/page.tsx
+++ src/app/app/learn/[itemSlug]/page.tsx
@@ -42,6 +42,18 @@ const TrainingItem = async ({ params }: { params: { itemSlug: string } }) => {
           </p>
         </div>
       </div>
+      <div className="flex flex-col w-full gap-4 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 justify-between">
+        {item.content.sections.map((section: any, index: number) => {
+          return (
+            <div className="flex flex-col gap-2" key={index}>
+              <h4 className="text-gray-900 text-lg font-semibold">
+                {section.heading}
+              </h4>
+              <p className="text-gray-600 font-light text-sm">{section.text}</p>
+            </div>
+          );
+        })}
+      </div>
     </Page>
   );
 };
