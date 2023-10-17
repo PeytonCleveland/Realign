2ec61ff43eb932d490bc7516668d2eef6fd6a5ed
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-17 15:56:27 -0500

    alignment

--- src/app/app/learn/[itemSlug]/page.tsx
+++ src/app/app/learn/[itemSlug]/page.tsx
@@ -44,7 +44,7 @@ const TrainingItem = async ({ params }: { params: { itemSlug: string } }) => {
         </div>
       </div>
       <div className="flex flex-col w-full gap-4 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 justify-between">
-        <div className="w-1/2 h-[250px] rounded-md shadow-sm overflow-hidden object-cover relative self-center">
+        <div className="w-2/5 h-[250px] rounded-md shadow-sm overflow-hidden object-cover relative self-end -mt-[100px]">
           <Image src={item.content.hero} alt={item.content.name} fill />
         </div>
         {item.content.sections.map((section: any, index: number) => {
