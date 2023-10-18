712dc34ed55c5d40c856cbe3cd47b341290bfc22
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-18 16:55:10 -0500

    icon

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -50,8 +50,9 @@ const AuthHome = async () => {
               <Link
                 href="/flagged-submissions.csv"
                 download
-                className="text-sm font-light text-blue-600 flex items-center gap-1 hover:underline underline-offset-4"
+                className="text-sm font-light text-blue-600 flex items-center gap-1.5 hover:underline underline-offset-4 decoration-blue-600"
               >
+                Flagged submissions
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
@@ -61,7 +62,6 @@ const AuthHome = async () => {
                   <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z" />
                   <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
                 </svg>
-                Flagged submissions
               </Link>
             </div>
             <button className="bg-blue-600 text-white disabled:bg-blue-300 px-5 py-2 font-medium rounded-md flex items-center gap-2 hover:bg-blue-500 focus:bg-blue-600 focus:ring-1 ring-blue-500 ring-offset-2 self-end">
