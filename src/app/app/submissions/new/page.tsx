e6b06427fc96360115c18ae7f5a14aed2528843c
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-10-18 10:04:50 -0500

    point change

--- src/app/app/submissions/new/page.tsx
+++ src/app/app/submissions/new/page.tsx
@@ -410,6 +410,12 @@ const NewSubmission = () => {
             onChange={(e) => handleResponseChange(e)}
           />
         </div>
+        <div className="flex flex-row-reverse justify-start items-center gap-2">
+          <h6 className="text-gray-900 font-semibold">
+            This Submission Earns:
+          </h6>
+          <p className="text-blue-600 font-semibold">1 Point</p>
+        </div>
         <div className="flex flex-row-reverse justify-start items-center gap-6">
           <button
             onClick={handleSubmit}
